c05f066b66fe3813a5ff754ba96ab09b2801277f
SamGombs20 <gombajosh18@gmail.com>
2026-03-04 14:21:41 +0300

    Update:Setting the user models

--- app/models/user.py
+++ app/models/user.py
@@ -1,28 +1,32 @@
 from uuid import UUID
 
-from pydantic import BaseModel
+from sqlmodel import SQLModel, Field
+from pydantic import EmailStr
 
+class UserBase(SQLModel):
+    first_name:str = Field(..., alias="firstName")
+    last_name:str = Field(..., alias="lastName")
+    church:str
+    phone_number:str = Field(..., alias="phoneNumber")
+    email:EmailStr
+    username:str
+    
+    class Config:
+        populate_by_name = True
+        from_attributes = True
 
-class UserLogIn(BaseModel):
+class User(UserBase, table=True):
+    __tablename__ = "analysis_users"
+    id:UUID = Field(default=None, primary_key=True)
+    hashed_password:str
+
+class UserLogIn(SQLModel):
     username:str
     password:str
     
-class UserRegister(BaseModel):
-    firstName:str
-    lastName:str
-    church:str
-    phoneNumber:str
-    email:str
-    username:str
+class UserRegister(UserBase):
     password:str
-    role:str
 
-class UserPublic(BaseModel):
+class UserPublic(UserBase):
     id:UUID
-    firstName:str
-    lastName:str
-    church:str
-    phoneNumber:str
-    email:str
-    username:str
     role:str
