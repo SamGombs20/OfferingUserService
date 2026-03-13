cba37bbbca2a667270501725fdb8c2791123fd97
SamGombs20 <gombajosh18@gmail.com>
2026-03-13 12:38:06 +0300

    Update:Getting the authenticated user success

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -52,7 +52,6 @@ def decode_token(token:str)->Optional[Dict[str, any]]:
     
 def get_current_user(token:str=Depends(oauth2_scheme))->str:
     try:
-        print(token)
         payload:Dict[str, any] = decode_token(token)
         token_data = TokenPayload(**payload)
         if datetime.fromtimestamp(token_data.exp) <datetime.utcnow():
