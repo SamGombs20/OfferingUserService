36634a59864de6c290c68ec2b4980fe1450b6ab2
SamGombs20 <gombajosh18@gmail.com>
2026-03-19 17:22:31 +0300

    Update:Resolving issues

--- app/config/config.py
+++ app/config/config.py
@@ -7,7 +7,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 class Settings(BaseSettings):
-    SECRET_KEY:str = SECRET_KEY
+    SECRET_KEY = SECRET_KEY
     ACCESS_TOKEN_EXPIRE_IN_MINUTES:int = 30
     REFRESH_TOKEN_EXPIRE_IN_DAYS:int=7
     ALGORITHM:str = "HS256"
